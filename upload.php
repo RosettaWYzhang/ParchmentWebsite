819cfb4d11aa46834b8e900039b8f1363c44436a
Hezser <contact.sergiohernandez@gmail.com>
2018-02-19 16:37:48 +0000

    Bugs

--- upload.php
+++ upload.php
@@ -9,11 +9,11 @@ echo "$total";
 //else{
 
 // Unique id for the process request
-// $uniqid = ( [ string $prefix = "" [, bool $more_entropy = FALSE ]] )
+ $id = uniqid (rand(), true);
 // $target_dir = "uploads/" . $uniqid . "/";
 $target_dir = "uploads/";
 echo "targetdir ";
-echo $target_dir;
+echo $id;
 if( is_dir($target_dir) === false ) // Should always be false, as it is a unique id
 {
     mkdir($target_dir);
