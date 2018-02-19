9496bbdd870bb93839374bfe3f4063966da11d18
Wanyue <RosettaWYzhang@users.noreply.github.com>
2018-02-19 12:22:28 +0000

    add variable db in config

--- config.php
+++ config.php
@@ -7,7 +7,7 @@ $db_name = 'login';
 
 //Establishes the connection
 $conn = mysqli_init();
-mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306);
+$db=mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306);
 if (mysqli_connect_errno($conn)) {
 die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
