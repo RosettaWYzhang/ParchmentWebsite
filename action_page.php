bf7c787bd06214c0fd3795bcc2b70f1e09f63453
Wanyue <RosettaWYzhang@users.noreply.github.com>
2018-02-20 14:32:00 +0000

    add exit

--- action_page.php
+++ action_page.php
@@ -27,6 +27,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
        session_start();
        $_SESSION['username'] = $myusername;
        header("location: services.php");
+       exit();
    }else {
       $error = "Your Login Name or Password is invalid";
       echo "login not successful";
