7db09f42ad58c410fd7f15cb2a64a25b605d7c09
Wanyue <RosettaWYzhang@users.noreply.github.com>
2018-02-21 12:04:24 +0000

    include register.php

--- register.php
+++ register.php
@@ -1,5 +1,6 @@
 <?php
 // Include config file
+ini_set('display_errors', 1);
 require_once 'config.php';
 
 // Define variables and initialize with empty values
@@ -128,7 +129,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                 <input type="submit" class="btn btn-primary" value="Submit">
                 <input type="reset" class="btn btn-default" value="Reset">
             </div>
-            <p>Already have an account? <a href="login.php">Login here</a>.</p>
+            <p>Already have an account? <a href="login.html">Login here</a>.</p>
         </form>
     </div>
 </body>
