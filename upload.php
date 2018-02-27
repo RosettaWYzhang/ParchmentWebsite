70041e96c29a635eee7b817c0ae3d347370f800a
Wanyue <RosettaWYzhang@users.noreply.github.com>
2018-02-27 15:57:08 +0000

    delete number

--- upload.php
+++ upload.php
@@ -72,7 +72,7 @@ for($i=0; $i<$total;$i++){
 
 if($successFile < 20){
     echo "Sorry, you need at least 20 successful images";
-8i887}
+}
 else{
     shell_exec('docker exec -d parchmentcontainer touch /services/Parchment/bundler_sfm/config8i887.txt');
     shell_exec('echo IMAGE_DIR=/services/Parchment/bundler_sfm/Images/' . ">> config$id.txt");
