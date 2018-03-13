9ec775b455bec80a1525856d21e837ee544f1dca
Wanyue <RosettaWYzhang@users.noreply.github.com>
2018-03-13 13:40:55 +0000

    jumbotron

--- register.php
+++ register.php
@@ -121,7 +121,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     <!-- Navigation -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
       <div class="container">
-        <a class="navbar-brand" href="#">3D parchment reconstruction</a>
+        <a class="navbar-brand" href="#">3D Parchment Reconstruction</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
